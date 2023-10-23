7bdc831e9d06f50fdb76e89d62d5ebd2d437c443
hisoakende <hisoakende@gmail.com>
2023-10-23 22:22:08 +0300

    Init

---
+++ src/test/java/ru/hisoakende/mycloud/MycloudApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hisoakende.mycloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
